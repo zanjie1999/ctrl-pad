4ec3260ebe022cd93696cec7f2b80e14f05f7dc2
zanjie1999 <zanjie1999@yeah.net>
2021-08-29 14:47:08 +0800

    vite-plugin-components

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,18 @@ import vue from '@vitejs/plugin-vue'
 import child_process from 'child_process'
 
 if (!process.env.NODE_ENV) {
-  process.env.VITE_DEV = "1";
-  child_process.exec("electron .");
+    process.env.VITE_DEV = "1";
+    child_process.exec("electron .");
 }
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "",
-  plugins: [vue()]
+    base: "",
+    plugins: [
+        vue(),
+        ViteComponents({
+            globalComponentsDeclaration: true,
+            customComponentResolvers: [VarletUIResolver()]
+        })
+    ]
 })
